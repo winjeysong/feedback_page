a37f98bb3ff405b2f4e9727fb981f62a70e6b9b5
winjeysong <winjey-song@163.com>
2017-12-01 17:31:45 +0800

    Init leftnav component

---
+++ src/components/LeftNav/LeftNav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './LeftNav.css';
+
+function LeftNav() {
+  return (
+    <div className={styles.normal}>
+      Component: LeftNav
+    </div>
+  );
+}
+
+export default LeftNav;
