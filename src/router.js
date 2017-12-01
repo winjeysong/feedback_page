efa9fc60f9085057886f11046d8023c132dd1165
winjeysong <winjey-song@163.com>
2017-12-01 17:33:59 +0800

    Add topitem route in router.js

--- src/router.js
+++ src/router.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
 
+import TopItem from "./routes/TopItem.js";
+
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
+        <Route path="/TopItem" component={TopItem} />
       </Switch>
     </Router>
   );
