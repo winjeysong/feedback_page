1f49524edf690f9f5087591368a59c45723cafca
winjeysong <winjey-song@163.com>
2017-12-03 14:34:37 +0800

    page jump

--- src/components/TopItem/TopItem.js
+++ src/components/TopItem/TopItem.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'dva/router';
 import styles from './TopItem.css';
 
-const TopItem = ({ iconPath, info }) => {
+
+const TopItem = ({ path, iconPath, info }) => {
   return (
     <div className={styles.TopItem}>
-      <a href="">
+      <Link to={path}>
         <img src={iconPath} alt={info} width="25" height="20" />
         <p className="">{info}</p>
-      </a>
+      </Link>
     </div>
   );
 };
 
 TopItem.propTypes = {
+  path: PropTypes.string.isRequired,
   iconPath: PropTypes.string.isRequired,
   info: PropTypes.string.isRequired,
 };
