e756d0af98483fbb5e649280085472dafb4a99ad
winjeysong <winjey-song@163.com>
2017-12-04 13:36:34 +0800

    Add topmenu component

---
+++ src/components/TopMenu/TopMenu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Menu } from 'antd';
+import TopItem from '../TopItem/TopItem';
+import styles from './TopMenu.css';
+import Pic1 from '../../assets/63F0209E-673E-4812-8DDB-4025171395B7@3x.png';
+import Pic2 from '../../assets/39D43ABF-538E-452C-AA96-45FBF859E525@3x.png';
+import Pic3 from '../../assets/71ACEA5D-F990-4089-8E8E-903A8725E606@3x.png';
+import Pic4 from '../../assets/AD8F6B3B-9993-462D-9F52-BF3EAE237C72@3x.png';
+
+function TopMenu() {
+  return (
+    <Menu
+      defaultSelectedKeys={['1']}
+      mode="horizontal"
+    >
+      <Menu.Item key="1"><TopItem path={'/推广运营'} iconPath={Pic1} info={'推广运营'} /></Menu.Item>
+      <Menu.Item key="2"><TopItem path={'/会员积分'} iconPath={Pic2} info={'会员积分'} /></Menu.Item>
+      <Menu.Item key="3"><TopItem path={'/页面管理'} iconPath={Pic3} info={'页面管理'} /></Menu.Item>
+      <Menu.Item key="4"><TopItem path={'/商场管理'} iconPath={Pic4} info={'商场管理'} /></Menu.Item>
+    </Menu>
+  );
+}
+
+export default TopMenu;
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
-import TopItemComponent from '../components/TopItem/TopItem';
+import TopMenuComponent from '../components/TopMenu/TopMenu';
 import LeftNavComponent from '../components/LeftNav/LeftNav';
-import Pic1 from '../assets/63F0209E-673E-4812-8DDB-4025171395B7@3x.png';
-import Pic2 from '../assets/39D43ABF-538E-452C-AA96-45FBF859E525@3x.png';
-import Pic3 from '../assets/71ACEA5D-F990-4089-8E8E-903A8725E606@3x.png';
-import Pic4 from '../assets/AD8F6B3B-9993-462D-9F52-BF3EAE237C72@3x.png';
+
 
 function IndexPage() {
   return (
     <div className={styles.normal}>
-      <ul>
-        <li><TopItemComponent path={'/推广运营'} iconPath={Pic1} info={'推广运营'} /></li>
-        <li><TopItemComponent path={'/会员积分'} iconPath={Pic2} info={'会员积分'} /></li>
-        <li><TopItemComponent path={'/页面管理'} iconPath={Pic3} info={'页面管理'} /></li>
-        <li><TopItemComponent path={'/商场管理'} iconPath={Pic4} info={'商场管理'} /></li>
-      </ul>
+      <TopMenuComponent />
       <LeftNavComponent />
     </div>
   );
