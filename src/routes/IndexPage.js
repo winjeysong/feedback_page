54be76e5362ee4a12f4a9b8d2014d5284e3c432f
winjeysong <winjey-song@163.com>
2017-12-07 13:15:06 +0800

    add layout

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import { connect } from 'dva';
-import styles from './IndexPage.css';
+import { Layout } from 'antd';
+import './IndexPage.scss';
 import TopMenuComponent from '../components/TopMenu/TopMenu';
 import LeftNavComponent from '../components/LeftNav/LeftNav';
 
+const { Header, Sider, Content } = Layout;
 
 function IndexPage() {
   return (
-    <div className={styles.normal}>
-      <TopMenuComponent />
-      <LeftNavComponent />
-    </div>
+    <Layout>
+      <Header className="header" style={{ height: 70, width: '100%', background: '#182a3a' }}>
+        <span className="logo">logo</span>
+        <TopMenuComponent />
+      </Header>
+      <Layout>
+        <Sider width={200} style={{ background: '#fff' }}>
+          <LeftNavComponent />
+        </Sider>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <p>意见反馈</p>
+          <Content style={{ background: '#f4f5f7', padding: 24, margin: 0, minHeight: 280 }}>
+            内容
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
   );
 }
 
