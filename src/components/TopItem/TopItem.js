235fa6ef042623010640ecac992f92fdf98497cb
winjeysong <winjey-song@163.com>
2017-12-11 15:31:40 +0800

    edit with styled-component

--- src/components/TopItem/TopItem.js
+++ src/components/TopItem/TopItem.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'dva/router';
-import styles from './TopItem.css';
+import styled from 'styled-components';
+import './TopItem.scss';
 
+const InfoWrap = styled.span`
+  height: max-content;
+  line-height: normal;
+  position: relative;
+  top: 1px;
+  opacity: 0.8;
+  font-size: 14px;
+  line-height: 14px;
+  font-family: sans-serif;
+  font-weight: 300;
+`;
 
 const TopItem = ({ path, iconPath, info }) => {
   return (
-    <div className={styles.TopItem}>
+    <span className="top-item">
       <Link to={path}>
-        <img src={iconPath} alt={info} width="25" height="20" />
-        <p className="">{info}</p>
+        <img src={iconPath} alt={info} width="33%" height="33%" />
+        <br />
+        <InfoWrap>{info}</InfoWrap>
       </Link>
-    </div>
+    </span>
   );
 };
 
