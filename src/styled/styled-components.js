c97ce22b65a3f44eae6341ac74f83018436b18c8
winjeysong <winjey-song@163.com>
2017-12-11 15:51:54 +0800

    add styled-components.js

---
+++ src/styled/styled-components.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const ContentTip = styled.p`
+  font-family: sans-serif;
+  font-size: 16px;
+  color: #1c4a70;
+  line-height: 16px;
+`;
+
+export const InfoWrap = styled.span`
+height: max-content;
+line-height: normal;
+position: relative;
+top: 1px;
+opacity: 0.8;
+font-size: 14px;
+line-height: 14px;
+font-family: sans-serif;
+font-weight: 300;
+`;
