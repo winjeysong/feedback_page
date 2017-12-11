ea4e980beb6bdedb5572e5c01fb4e3f1d13c2d39
winjeysong <winjey-song@163.com>
2017-12-11 15:52:28 +0800

    change the style with styled-component

--- src/components/TopItem/TopItem.js
+++ src/components/TopItem/TopItem.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'dva/router';
-import styled from 'styled-components';
+import { InfoWrap } from '../../styled/styled-components';
 import './TopItem.scss';
-
-const InfoWrap = styled.span`
-  height: max-content;
-  line-height: normal;
-  position: relative;
-  top: 1px;
-  opacity: 0.8;
-  font-size: 14px;
-  line-height: 14px;
-  font-family: sans-serif;
-  font-weight: 300;
-`;
 
 const TopItem = ({ path, iconPath, info }) => {
   return (
