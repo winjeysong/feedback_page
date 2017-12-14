d5fa2e7f35372d13db2602fb5ad1d0a859965241
winjeysong <winjey-song@163.com>
2017-12-15 04:15:20 +0800

    init new component formwrapper

---
+++ src/components/FormWrapper/FromWrapper.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './FromWrapper.css';
+
+function FromWrapper() {
+  return (
+    <div className={styles.normal}>
+      Component: FromWrapper
+    </div>
+  );
+}
+
+export default FromWrapper;
