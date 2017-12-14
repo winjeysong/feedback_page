7649e9970344f56bc03f42412c1161d9b1c905f4
winjeysong <winjey-song@163.com>
2017-12-14 21:59:46 +0800

    changes
    1. change pics to 1x
    2. change left distance

--- src/components/TopMenu/TopMenu.js
+++ src/components/TopMenu/TopMenu.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Menu } from 'antd';
 import TopItem from '../TopItem/TopItem';
-import Pic1 from '../../assets/63F0209E-673E-4812-8DDB-4025171395B7@3x.png';
-import Pic2 from '../../assets/39D43ABF-538E-452C-AA96-45FBF859E525@3x.png';
-import Pic3 from '../../assets/71ACEA5D-F990-4089-8E8E-903A8725E606@3x.png';
-import Pic4 from '../../assets/AD8F6B3B-9993-462D-9F52-BF3EAE237C72@3x.png';
+import Pic1 from '../../assets/63F0209E-673E-4812-8DDB-4025171395B7@1x.png';
+import Pic2 from '../../assets/39D43ABF-538E-452C-AA96-45FBF859E525@1x.png';
+import Pic3 from '../../assets/71ACEA5D-F990-4089-8E8E-903A8725E606@1x.png';
+import Pic4 from '../../assets/AD8F6B3B-9993-462D-9F52-BF3EAE237C72@1x.png';
 
 function TopMenu() {
   return (
     <Menu
       defaultSelectedKeys={['1']}
       mode="horizontal"
-      style={{ background: 'none', position: 'absolute', top: 0, left: 150, width: 'max-content', height: '70px', textAlign: 'center', lineHeight: 'normal' }}
+      style={{ background: 'none', position: 'absolute', top: 0, left: 205, width: 'max-content', height: '70px', textAlign: 'center', lineHeight: 'normal', borderBottom: 'none' }}
     >
       <Menu.Item key="1"><TopItem path={'/推广运营'} iconPath={Pic1} info={'推广运营'} /></Menu.Item>
       <Menu.Item key="2"><TopItem path={'/会员积分'} iconPath={Pic2} info={'会员积分'} /></Menu.Item>
