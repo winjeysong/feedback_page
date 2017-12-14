df8ce29b1ff3550cbb19d0fdaa4e20d86a0a8b2a
winjeysong <winjey-song@163.com>
2017-12-14 17:48:00 +0800

    gather all custom menu styles

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { connect } from 'dva';
 import { Layout } from 'antd';
-import './IndexPage.scss';
 import TopMenuComponent from '../components/TopMenu/TopMenu';
 import LeftNavComponent from '../components/LeftNav/LeftNav';
 import { ContentTip } from '../styled/styled-components';
+import '../styled/menu.less';
 
 const { Header, Sider, Content } = Layout;
 
@@ -19,9 +19,9 @@ function IndexPage() {
         <Sider style={{ background: '#fff', width: 200 }}>
           <LeftNavComponent />
         </Sider>
-        <Layout style={{ padding: '0 24px 24px' }}>
+        <Layout style={{ background: '#f4f5f7', padding: '56px 26px 100px 26px' }}>
           <ContentTip>意见反馈</ContentTip>
-          <Content style={{ background: '#f4f5f7', padding: 24, margin: 0, minHeight: 280 }}>
+          <Content style={{ padding: 24, margin: 0, minHeight: 280 }}>
             内容
           </Content>
         </Layout>
