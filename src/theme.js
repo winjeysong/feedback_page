7f898a9c52aa24b20c72dcd9fd01c21753f3d2bc
winjeysong <winjey-song@163.com>
2017-12-21 20:24:36 +0800

    add

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,14 @@
 module.exports = () => {
   return {
+    // --------- Color
     '@primary-color': '#fe6900',
+
+    // --------- Link
     '@link-active-color': '#fe6900',
     '@link-hover-color': '#fe6900',
-    '@menu-collapsed-width': '40px',
+
+    // -------- Form
+    '@label-color': '#666666',
+
   };
 };
