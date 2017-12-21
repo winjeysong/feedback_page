3e4f4801ab9946045dd70a4430b5be6d36e74e96
winjeysong <winjey-song@163.com>
2017-12-21 20:23:50 +0800

    add configbox component

---
+++ src/components/ConfigBox/ConfigBox.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Row, Col, Form, Button } from 'antd';
+import { ContentWrap } from '../../styled/styled-components';
+
+const FormItem = Form.Item;
+
+function ConfigBox() {
+  return (
+    <ContentWrap center>
+      <Form>
+        <Row>
+          <Col span={12} offset={6}>
+            <FormItem>
+              <Button type="primary" ghost style={{ fontSize: 12, padding: '0 27px' }}>配置标签</Button>
+            </FormItem>
+          </Col>
+        </Row>
+      </Form>
+      <Form>
+        <Row>
+          <Col span={12} offset={6}>
+            <FormItem>
+              <Button type="primary" ghost style={{ fontSize: 12 }}>配置快捷回复</Button>
+            </FormItem>
+          </Col>
+        </Row>
+      </Form>
+    </ContentWrap>
+  );
+}
+
+export default ConfigBox;
