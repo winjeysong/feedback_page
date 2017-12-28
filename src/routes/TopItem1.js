51b12a2b1c4fd822c1a1809ab34c769ae945311d
winjeysong <winjey-song@163.com>
2017-12-28 14:05:32 +0800

    edit topitem1 route with mainlayout

--- src/routes/TopItem1.js
+++ src/routes/TopItem1.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { connect } from 'dva';
-import styles from './TopItem1.css';
+import MainLayout from '../components/MainLayout/MainLayout';
 
-function TopItem1() {
+function TopItem1({ location }) {
+  const content = <h1>推广运营页面</h1>;
   return (
-    <div className={styles.normal}>
-      推广运营
-    </div>
+    <MainLayout location={location} content={content} />
   );
 }
 
