590cfa1f8839cdfec984e01277d6c781d521946c
winjeysong <winjey-song@163.com>
2017-12-28 16:45:56 +0800

    add new prop avatar according to userwrap

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { UserWrap } from '../../styled/styled-components';
 
 function UserInfo({ userInfo }) {
-  const { name, tele, card } = userInfo;
+  const { name, tele, card, avatar } = userInfo;
   return (
-    <ul className="user-info">
-      <li className="name">{name}</li>
-      <li className="tele">{tele}</li>
-      <li className="card">{card}</li>
-    </ul>
+    <UserWrap avatar={avatar}>
+      <ul className="user-info">
+        <li className="name">{name}</li>
+        <li className="tele">{tele}</li>
+        <li className="card">{card}</li>
+      </ul>
+    </UserWrap>
   );
 }
 
